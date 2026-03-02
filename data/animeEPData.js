70342e58b7fe10779ee7613552d1d5ab0c1e6efa
Gongbang-lab <gongbang0312@gmail.com>
2026-03-02 18:29:37 +0900

    fixed design

---
+++ data/animeEPData.js
@@ -0,0 +1,118 @@
+const animeEPData = {
+    "57658": [
+    {
+        "episode no": "제48화",
+        "episode title": "집행",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        }
+    },
+    {
+        "episode no": "제49화",
+        "episode title": "다시 한번",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "타카다 요스케"
+            ]
+        }
+    },
+    {
+        "episode no": "제50화",
+        "episode title": "사멸회유에 대하여",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "이치카와 타쿠미"
+            ]
+        }
+    },
+    {
+        "episode no": "제51화",
+        "episode title": "갈대를 입에 물다.",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "고쇼조노 쇼타",
+                "스즈키 리사",
+                "스노우치 유스케"
+            ]
+        }
+    },
+    {
+        "episode no": "제52화",
+        "episode title": "열기",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "안도 마사오미"
+            ]
+        }
+    },
+    {
+        "episode no": "제53화",
+        "episode title": "부품",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "키도코로 세이메이"
+            ]
+        }
+    },
+    {
+        "episode no": "제54화",
+        "episode title": "도쿄 제1결계①",
+        "storyboard": {
+            "staff": [
+                "고쇼조노 쇼타"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "사토 타케루"
+            ]
+        }
+    },
+    {
+        "episode no": "제55화",
+        "episode title": "도쿄 제1결계②",
+        "storyboard": {
+            "staff": [
+                "타카다 요스케"
+            ]
+        },
+        "episode director": {
+            "staff": [
+                "타카다 요스케"
+            ]
+        }
+    }
+],
+}
