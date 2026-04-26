233434355238eef5b473b6eadc41b58a5bedf6bd
Gongbang-lab <gongbang0312@gmail.com>
2026-04-26 20:06:19 +0900

    Updated 2 Quarter Meme

--- data/animeMemeData.js
+++ data/animeMemeData.js
@@ -1,7 +1,7 @@
 const AnimeMemeData = [
     {
         "id": "meme_001",
-        "name": "나오야 전투",
+        "name": "나오야 머리쓰다듬기",
         "type": "gif", // image, gif, video 구분
         "src1": "image/meme/나오야 전투.gif",
         "origin": "주술회전 3기",
@@ -40,5 +40,55 @@ const AnimeMemeData = [
         "src2": "image/meme/똥 쌌다고_한국어.mp4",
         "origin": "죠죠 스틸볼런",
         "quarter": "1분기"
+    },
+    {
+        "id": "meme_006",
+        "name": "루루카 변신",
+        "type": "video",
+        "src1": "image/meme/프리큐어 루루카 변신.mp4",
+        "origin": "명탐정 프리큐어",
+        "quarter": "1분기"
+    },
+    {
+        "id": "meme_007",
+        "name": "나 화남",
+        "type": "webp",
+        "src1": "image/meme/용사파티.webp",
+        "origin": "용사 파티에 귀여운 애가 있어서, 고백해봤다.",
+        "quarter": "1분기"
+    },
+    {
+        "id": "meme_008",
+        "name": "액션의 사망 유희.",
+        "type": "gif",
+        "src1": "image/meme/액션의 사망유희.gif",
+        "origin": "사망 유희로 밥을 먹는다.",
+        "quarter": "1분기"
+    },
+    {
+        "id": "meme_009",
+        "name": "메리지 톡신",
+        "type": "webp",
+        "src1": "image/meme/메리지 톡신.webp",
+        "origin": "메리지 톡신",
+        "quarter": "2분기"
+    },
+    {
+        "id": "meme_010",
+        "name": "카즈야 망상",
+        "type": "mp4",
+        "src1": "image/meme/카즈야 망상.mp4",
+        "src2_title": "⚠️후방 주의⚠️",
+        "src2": "image/meme/카즈야 망상 2.webp",
+        "origin": "여친, 빌리겠습니다 5기",
+        "quarter": "2분기"
+    },
+    {
+        "id": "meme_011",
+        "name": "속이 뻥",
+        "type": "webp",
+        "src1": "image/meme/속이 뻥.webp",
+        "origin": "여친, 빌리겠습니다 5기",
+        "quarter": "2분기"
     }
 ];
